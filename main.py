ee3b2220037c02bb146d9c181d4bb9a13d665f6d
Ryan Mello <ryanmello897@gmail.com>
2025-04-15 15:49:07 -0700

    simulate post req

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI
+from fastapi import *
 
 app = FastAPI()
 
 @app.get("/")
-def home():
+def root():
     return { "message" : "Hello World" }
 
+@app.post("/posts")
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return { "message" : "Post created!" }
